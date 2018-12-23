556250de0ef19a234561b8d42ffc7a3bc9e14640
Tomasz Bieniek <tomek-1996-1996@o2.pl>
2018-12-23 13:45:52 +0100

    renaming modules

--- node-server-app/api/routes/users.js
+++ node-server-app/api/routes/users.js
@@ -2,15 +2,23 @@ const express = require("express")
 const router = express.Router()
 const User = require("../models/user")
 
-router.get("/:userId", (request, response) => {
+router.get("/:userId", (request, response, next) => {
     const id = request.params.userId
     User.findByPk(id).then(user => {
         if (user === null) {
-            throw new Error("User not found")
+            next(new Error("User not found"))
         } else {
             response.status(200).json(user)
         }
     }).tapCatch("Database connection error")
 })
+
+router.post("/signup", (request, response) =>{
+    const body = request.body
+    console.log(body)
+})
+
+
+
 
 module.exports = router
