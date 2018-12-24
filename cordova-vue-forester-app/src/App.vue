dbb530c70736eadd65410b7d6aa4d308fad59a8d
Tomasz Bieniek <tomek-1996-1996@o2.pl>
2018-12-25 00:16:35 +0100

    Added welcome splash-screen on Android devices

--- cordova-vue-forester-app/src/App.vue
+++ cordova-vue-forester-app/src/App.vue
@@ -9,25 +9,12 @@ export default {
 };
 </script>
 
-<style lang="scss">
-
-@import url('https://fonts.googleapis.com/css?family=Roboto:300,400,700,900&subset=latin-ext');
+<style lang="scss" >
+@import url("https://fonts.googleapis.com/css?family=Roboto:300,400,700,900&subset=latin-ext");
 $font-weight-light: 300;
 $font-weight-normal: 400;
-$font-weight-bold: 600;
-$font-weight-black: 800;
-
-#app {
-  background: url("./assets/img/bg_forest.jpg");
-  position: fixed;
-  width: 100%;
-  height: 100vh;
-  top: 0;
-  z-index: -1;
-  background-repeat: no-repeat;
-  background-size: cover;
-  background-position: 50% 85%;
-}
+$font-weight-bold: 700;
+$font-weight-black: 900;
 
 * {
   box-sizing: border-box;
@@ -39,4 +26,17 @@ body {
   padding: 0;
   font-family: "Roboto", sans-serif;
 }
+/*
+#app {
+  background: url("./assets/img/bg_forest.jpg");
+  position: fixed;
+  width: 100%;
+  height: 100vh;
+  top: 0;
+  z-index: -1;
+  background-repeat: no-repeat;
+  background-size: cover;
+  background-position: 50% 85%;
+}
+*/
 </style>
