0bc31153f5233fa8722a0264c2db345ea243726e
dariusz szymczak <dariusz.szymczak96@gmail.com>
2019-01-12 19:00:44 +0100

    Vuetify install

--- vue-cordova-forester-app/src/main.js
+++ vue-cordova-forester-app/src/main.js
@@ -2,12 +2,10 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import BootstrapVue from 'bootstrap-vue'
-import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap-vue/dist/bootstrap-vue.css'
+import Vuetify from 'vuetify'
 
-Vue.use(BootstrapVue)
 Vue.config.productionTip = false
+Vue.use(Vuetify)
 
 new Vue({
   router,
