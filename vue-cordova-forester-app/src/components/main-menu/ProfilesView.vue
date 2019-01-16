ef24750c3a88be1404d024e4b110718e838d7302
Tomasz Bieniek <tomek-1996-1996@o2.pl>
2019-01-16 23:24:33 +0100

    Added new flat buttons in main menu

--- vue-cordova-forester-app/src/components/main-menu/ProfilesView.vue
+++ vue-cordova-forester-app/src/components/main-menu/ProfilesView.vue
@@ -1,15 +1,15 @@
 <template>
         <v-container grid-list-xs>
-            <RotateLogo/>
+            <rotate-logo/>
                 <v-layout class="white--text font-weight-medium" align-center column fill-height>
                         <v-flex xs1 mt-5>
                             <v-btn class="responsive-btn" color="success" to="/enter-the-forest"  >Wejdź do lasu</v-btn>
                         </v-flex>
                         <v-flex xs1>
-                            <v-btn class="responsive-btn" color="success" v-bind:loading="loading1" v-on:click="loading1 = !loading1"> Zaloguj się</v-btn>
+                            <v-btn class="responsive-btn"  color="success"> Zaloguj się</v-btn>
                         </v-flex>
-                        <p>Nie masz konta ? Zarejestruj się !</p>
-                        <p>Przypomnij hasło</p>                    
+                        <v-btn class="responsive-btn pa-2 font-weight-normal" color="white" flat>Zarejestruj się !</v-btn>
+                        <v-btn class="responsive-btn pa-2 font-weight-normal"  color="white" flat> Przypomnij hasło</v-btn>    
                 </v-layout>
         </v-container>
 </template>
@@ -18,14 +18,15 @@
 import RotateLogo from "../common/RotateLogo"
 export default {
     name: "ProfilesView",
+    components:{
+        'rotate-logo' : RotateLogo
+    },
     data(){
         return{
-            loading1: false,
+        
         }
     }, 
-    components:{
-        'RotateLogo' : RotateLogo
-    }
+
 }
 </script>
 
@@ -35,9 +36,4 @@ export default {
         height: 4vh;
         font-size: 3vw;
     }
-
-    p {
-        font-size: 3vw;
-    }
-
 </style>
