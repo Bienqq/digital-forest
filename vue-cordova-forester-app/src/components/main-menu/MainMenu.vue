15f056cd5dc5070be87f80e39baad375a6db87aa
dariusz szymczak <dariusz.szymczak96@gmail.com>
2019-01-29 11:49:09 +0100

    Reg Form validation

--- vue-cordova-forester-app/src/components/main-menu/MainMenu.vue
+++ vue-cordova-forester-app/src/components/main-menu/MainMenu.vue
@@ -1,64 +1,66 @@
 <template>
-<v-container fluid v-on:click.self="hideRegisterForm()">
-            <rotate-logo v-if="showLogo"/>
-                <v-layout class="white--text font-weight-medium top-spacer" align-center column  >
-                        <template >
-                           <v-card width="90%" class="light-gray">
-                          <v-flex xs10 offset-xs1 class="top-spacer">
-                              <v-btn block xs12  color="success" to="/enter-the-forest">Wejdź do lasu</v-btn>
-                          </v-flex>
-                          <v-flex xs10 offset-xs1  class="bottom-spacer">
-                              <v-btn block  color="success" > Zaloguj się</v-btn>
-                          </v-flex>
-                            <v-divider ></v-divider>
-                          <v-flex justify-center xs8 offset-xs2 >
-                             <v-btn block  small color="black" flat v-on:click.stop="showRegister = true">Zarejestruj się !</v-btn>
-                          </v-flex>
-                          <!-- <v-flex justify-center xs8 offset-xs2 >
+  <v-container fluid v-on:click.self="hideRegisterForm()">
+    <rotate-logo v-if="showLogo" />
+    <v-layout class="white--text font-weight-medium top-spacer" align-center column>
+      <template>
+        <v-card width="90%" class="light-gray">
+          <v-flex xs10 offset-xs1 class="top-spacer">
+            <v-btn block xs12 color="success" to="/enter-the-forest">Wejdź do lasu</v-btn>
+          </v-flex>
+          <v-flex xs10 offset-xs1 class="bottom-spacer">
+            <v-btn block color="success"> Zaloguj się</v-btn>
+          </v-flex>
+          <v-divider></v-divider>
+          <v-flex justify-center xs8 offset-xs2>
+            <v-btn block small color="black" flat v-on:click.stop="showRegister = true">Zarejestruj się !</v-btn>
+          </v-flex>
+          <!-- <v-flex justify-center xs8 offset-xs2 >
                             <v-btn block small color="black" flat> Przypomnij hasło</v-btn> 
                           </v-flex> -->
-                           </v-card>
-                          <v-dialog fullscreen v-model="showRegister"> <Register-form v-on:closeWindow="showRegister = false"/> </v-dialog>
-                       </template>
-                </v-layout>
-    </v-container>
+        </v-card>
+        <v-dialog fullscreen v-model="showRegister">
+          <Register-form v-on:closeWindow="showRegister = false" />
+        </v-dialog>
+      </template>
+    </v-layout>
+  </v-container>
 </template>
 
 <script>
-import RotateLogo from "../common/RotateLogo";
-import RegisterForm from "./RegisterForm";
-export default {
-  name: "MainMenu",
-  components: {
-    "rotate-logo": RotateLogo,
-    "Register-form": RegisterForm
-  },
-  data() {
-    return {
-      RegisterComponent: "Register-form",
-      showRegister: false,
-      showLogo : true
-    };
-  },
-  methods: {
-    showRegisterForm() {
-      this.showRegister = true;
-      this.showLogo = false;
+  import RotateLogo from "../common/RotateLogo";
+  import RegisterForm from "./RegisterForm";
+  export default {
+    name: "MainMenu",
+    components: {
+      "rotate-logo": RotateLogo,
+      "Register-form": RegisterForm
     },
+    data() {
+      return {
+        RegisterComponent: "Register-form",
+        showRegister: false,
+        showLogo: true
+      };
+    },
+    methods: {
+      showRegisterForm() {
+        this.showRegister = true;
+        this.showLogo = false;
+      },
 
-    hideRegisterForm() {
-      if (this.showRegister) {
-        this.showRegister = false;
-        this.showLogo = true;
+      hideRegisterForm() {
+        if (this.showRegister) {
+          this.showRegister = false;
+          this.showLogo = true;
+        }
       }
+    },
+    created: function () {
+      window.addEventListener("keyboardWillShow", () => {
+        document.activeElement.scrollIntoViewIfNeeded()
+      });
     }
-  },
-  created: function() {
-    window.addEventListener("keyboardWillShow", () => {
-      document.activeElement.scrollIntoViewIfNeeded()
-    });
-  }
-};
+  };
 </script>
 
 <style scoped lang="scss">
--- vue-cordova-forester-app/src/components/main-menu/RegisterForm.vue
+++ vue-cordova-forester-app/src/components/main-menu/RegisterForm.vue
@@ -14,82 +14,48 @@
             </v-toolbar>
 
             <v-flex xs12>
-              <v-select
-                color="green" 
-                box
-                :items="typyKont"
-                label="Wybierz typ konta"
-                v-model="accountType"
-              ></v-select>
+              <v-select color="green" box :items="typyKont" label="Wybierz typ konta" v-model="accountType"></v-select>
             </v-flex>
 
             <v-flex xs10 offset-xs1>
-              <v-text-field
-              color="green" 
-                v-model="login"
-                prepend-inner-icon="person"
-                v-bind:rules="nameRules"
-                v-bind:counter="20"
-                label="Login"
-                required
-              ></v-text-field>
+              <v-text-field color="green" v-model="login" prepend-inner-icon="person" v-bind:rules="loginRules"
+                v-bind:counter="20" label="Login" required></v-text-field>
             </v-flex>
 
-            <!-- <v-flex xs10 offset-xs1>
-              <v-text-field
-                v-model="firstname"
-                v-bind:rules="nameRules"
-                v-bind:counter="15"
-                label="Imie"
-                required
-              ></v-text-field>
-            </v-flex>
+
+            <div v-if="accountType === 'Leśniczy'">
+              <v-flex xs10 offset-xs1>
+                <v-text-field prepend-inner-icon="person_outline" v-model="firstname" v-bind:rules="nameRules" v-bind:counter="15" label="Imie" required></v-text-field>
+              </v-flex>
+
+              <v-flex xs10 offset-xs1>
+                <v-text-field prepend-inner-icon="person_outline" v-model="lastname" v-bind:rules="surnameRules" v-bind:counter="30" label="Nazwisko" required></v-text-field>
+              </v-flex>
+            </div>
 
             <v-flex xs10 offset-xs1>
-              <v-text-field
-                v-model="lastname"
-                v-bind:rules="nameRules"
-                v-bind:counter="30"
-                label="Nazwisko"
-                required
-              ></v-text-field>
-            </v-flex>-->
-            <v-flex xs10 offset-xs1>
-              <v-text-field color="green" prepend-inner-icon="email" v-model="email" v-bind:rules="emailRules" label="E-mail" required></v-text-field>
+              <v-text-field color="green" prepend-inner-icon="email" v-model="email" v-bind:rules="emailRules" label="E-mail"
+                required></v-text-field>
             </v-flex>
 
             <v-flex xs10 offset-xs1>
-              <v-text-field
-                v-model="password"
-                color="green" 
-                prepend-inner-icon="lock"
-                v-bind:rules="passwordRules"
-                v-bind:counter="30"
-                label="Hasło"
-                required
-              ></v-text-field>
+              <v-text-field v-model="password" color="green" prepend-inner-icon="lock" v-bind:rules="passwordRules"
+                v-bind:counter="30" label="Hasło" type="password" required></v-text-field>
             </v-flex>
 
             <v-flex xs10 offset-xs1>
-              <v-text-field
-                v-model="repeatPassword"
-                color="green" 
-                prepend-inner-icon="lock"
-                v-bind:rules="repeatPassword"
-                v-bind:counter="30"
-                label="Powtórz Hasło"
-                required
-              ></v-text-field>
+              <v-text-field v-model="repeatPassword" color="green" prepend-inner-icon="lock" v-bind:rules="repeatPasswordRules"
+                v-bind:counter="30" label="Powtórz Hasło" type="password" required></v-text-field>
             </v-flex>
 
             <v-flex xs10 offset-xs1>
-              <v-checkbox v-model="checkbox" color="green" >
+              <v-checkbox  color="green">
                 <div slot="label">Zgadzam się na przetwarzanie danych dla celów rejestracyjnych.</div>
               </v-checkbox>
             </v-flex>
 
             <v-flex xs10 offset-xs1>
-              <v-checkbox v-model="checkbox" color="green" >
+              <v-checkbox color="green">
                 <div slot="label">Akceptuję Regulamin</div>
               </v-checkbox>
             </v-flex>
@@ -105,36 +71,56 @@
 </template>
 
 <script>
-export default {
-  data() {
-    return {
-      valid: false,
-      firstname: "",
-      lastname: "",
-      accountType: "Użytkownik",
-      nameRules: [
-        v => !!v || "Pole wymagane",
-        v => v.length <= 15 || "Imie nie może zawierać więcej niż 15"
-      ],
-      email: "",
-      emailRules: [
-        v => !!v || "Pole wymagane",
-        v => /.+@.+/.test(v) || "Błędnie wpisany E-mail"
-      ],
-      typyKont: ["Użytkownik", "Leśniczy"]
-    };
-  },
-  methods: {
-    closeWindow() {
-      this.$emit("closeWindow");
+  export default {
+    data() {
+      return {
+        valid: false,
+        firstname: "",
+        lastname: "",
+        login:"",
+        password:"",
+        repeatPassword:"",
+        accountType: "Użytkownik",
+        nameRules: [
+          v => !!v || "Pole wymagane",
+          v => v.length <= 15 || "Imie nie może zawierać więcej niż 15"
+        ],
+        surnameRules: [
+          v => !!v || "Pole wymagane",
+          v => v.length <= 15 || "Imie nie może zawierać więcej niż 15"
+        ],
+        loginRules: [
+          v => !!v || "Pole wymagane",
+          v => v.length <= 15 || "Login nie może zawierać więcej niż 20"
+        ],
+        passwordRules: [
+          v => !!v || "Pole wymagane",
+          v => v.length <= 15 || "Hasło nie może zawierać więcej niż 30",
+          v => /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{8,}$/.test(v) ||
+          "Hasło musi zawierać min. 8 znaków, jedną dużą litere i cyfrę"
+        ],
+        repeatPasswordRules: [
+          v => v === this.password || "Hasło powinno być takie samo jak poprzednie."
+        ],
+        email: "",
+        emailRules: [
+          v => !!v || "Pole wymagane",
+          v => /.+@.+/.test(v) || "Błędnie wpisany E-mail"
+        ],
+        typyKont: ["Użytkownik", "Leśniczy"]
+      };
+    },
+    methods: {
+      closeWindow() {
+        this.$emit("closeWindow");
+      }
+    },
+    computed: {
+      accountTypeComputed: function () {
+        return this.accountType == "Użytkownik" ? "Użytkownika" : "Leśniczego";
+      }
     }
-  },
-  computed: {
-    accountTypeComputed: function() {
-      return this.accountType == "Użytkownik" ? "Użytkownika" : "Leśniczego";
-    }
-  }
-};
+  };
 </script>
 
 <style scoped lang="scss">
